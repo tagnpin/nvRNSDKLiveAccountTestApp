988723de30d257715a6e3fbefd61abcf212725e7
Neeraj <48247394+neeraj123123@users.noreply.github.com>
2025-11-18 10:24:57 +0530

    Update MainApplication.kt

--- android/app/src/main/java/com/notifyvisitors/rn/nvrnsdkliveaccounttestapp/MainApplication.kt
+++ android/app/src/main/java/com/notifyvisitors/rn/nvrnsdkliveaccounttestapp/MainApplication.kt
@@ -25,8 +25,11 @@ class MainApplication : Application(), ReactApplication {
   //  private val NOTIFYVISITORS_BRAND_ENCRYPTION_KEY: String = "515CBDE82C402BDD85E4DFCCFD8904F6"
   //  private val NOTIFYVISITORS_BRAND_ID: Int = 8115
 
-    private val nvBrandID = BuildConfig.NV_BRAND_ID             // Int
+    private val nvBrandID: Int = BuildConfig.NV_BRAND_ID.toInt()             // Int
     private val nvSecreKey = BuildConfig.NV_BRAND_ENCRYPTION_KEY  // String
+
+    println("Brand ID: $brandId")
+    println("Encryption Key: $encryption")
 
   //  private val nvBrandID = if (BuildConfig.DEBUG) 7577 else 8115
  //   private val nvSecreKey = if (BuildConfig.DEBUG) "DB52A5B00BB0D3BF426639A1B9FCF2F7" else "515CBDE82C402BDD85E4DFCCFD8904F6"
